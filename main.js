7aa4436ebb869ca19dab23baad17400e5979d2b8
ip0705w <99403855+ip0705w@users.noreply.github.com>
2022-02-24 17:56:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-leftWristX=0
-leftWristY=0
-rigthWristX=0
-rigthWristY=0
+leftWristX=0;
+leftWristY=0;
+rigthWristX=0;
+rigthWristY=0;
+scoreLeftWrist=0;
 
 function preload(){
   sound=loadSound("music.mp3");
@@ -26,9 +27,11 @@ function modelLoaded(){
 function draw(){
   image(video,0,0,400,400);
   
+  
+  
 }
 function start(){
-  sound.play();
+  sound.play()
   sound.setVolume(1);
   sound.rate(1);
 }
@@ -43,5 +46,28 @@ function gotPoses(results){
     rightWristX= results[0].pose.rightWrist.x;
     rightWristY= results[0].pose.rightWrist.y;
     console.log("rightwristX= " +rightWristX + "rightWristY= " + rightWristY);
+   
+    scoreLeftWrist=results[0].pose.keypoints[9].score;
+    console.log("scoreLeftwrist= " +scoreLeftWrist)
   }
 }
+
+function draw(){
+  image(video,0,0,400,400);
+  
+  fill("#B71C1C");
+  stroke("#212121");
+  
+  if(scoreLeftWrist > 0.2)
+    {
+      circle(leftWristX,leftWristY,20);
+      number1=Number(leftWristY);
+      floor1=floor(number1);
+      volume=floor1/400;
+      sound.setVolume(volume)
+      console.log(volume);
+      document.getElementById("vol").innerHTML="volume= " +volume;
+      
+    }
+}
+
