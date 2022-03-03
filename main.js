1bdc8efb86ff4ff86d0c5b85aecc97f96b44b1ec
ip0705w <99403855+ip0705w@users.noreply.github.com>
2022-03-03 18:32:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristY=0;
 rigthWristX=0;
 rigthWristY=0;
 scoreLeftWrist=0;
+scoreRigthWrist=0;
 
 function preload(){
   sound=loadSound("music.mp3");
@@ -24,12 +25,7 @@ function modelLoaded(){
   console.log('PoseNet is Loaded');
 }
 
-function draw(){
-  image(video,0,0,400,400);
-  
-  
-  
-}
+
 function start(){
   sound.play()
   sound.setVolume(1);
@@ -48,7 +44,10 @@ function gotPoses(results){
     console.log("rightwristX= " +rightWristX + "rightWristY= " + rightWristY);
    
     scoreLeftWrist=results[0].pose.keypoints[9].score;
-    console.log("scoreLeftwrist= " +scoreLeftWrist)
+    console.log("scoreLeftwrist= " +scoreLeftWrist);
+    
+    scoreRigthWrist=results[0].pose.keypoints[10].score;
+
   }
 }
 
@@ -64,10 +63,33 @@ function draw(){
       number1=Number(leftWristY);
       floor1=floor(number1);
       volume=floor1/400;
-      sound.setVolume(volume)
+    sound.setVolume(volume)
       console.log(volume);
       document.getElementById("vol").innerHTML="volume= " +volume;
       
     }
+  if(scoreRigthWrist>0.2){
+    circle(rigthWristX,rigthWristY,20);
+    if(rigthWristY>0 && rigthWristY < 80){
+      sound.rate(0.5)
+      document.getElementByID("speed").innerHTML="speed=0.5"
+    }
+    else if(rigthWristY>80 && rigthWristY<160){
+      sound.rate(1)
+      document.getElementById("speed").innerHTML="speed=1"
+    }
+    else if(rigthWristY>160 && rigthWristY<240){
+      sound.rate(1.5)
+      document.getElementById("speed").innerHTML="speed=1.5"
+    }
+    else if(rigthWristY>240 && rigthWristY<320){
+      sound.rate(2)
+      document.getElementById("speed").innerHTML="speed=2"
+    }
+    else if(rigthWristY>320 && rigthWristY<400){
+      sound.rate(2.5)
+      document.getElementById?("speed").innerHTML="speed=2.5"
+    }
+  }
 }
 
